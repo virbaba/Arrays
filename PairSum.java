0588f6999d4a34c065f2930c9fa86823f5ff133e
virbaba <109612471+virbaba@users.noreply.github.com>
2023-10-13 10:56:39 +0530

    Create PairSum.java

---
+++ PairSum.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.* ;
+
+public class Solution{
+    public static List<int[]> pairSum(int[] arr, int s) {
+        // Write your code here.
+        List<int[]> pairs = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == s) {
+                  // if the pair is (3, -3) then it become (-3,3) by below code
+                    int[] pair = {Math.min(arr[i], arr[j]), Math.max(arr[i], arr[j])};
+                    pairs.add(pair);
+                }
+            }
+        }
+
+        pairs.sort((a, b) -> {
+            if (a[0] != b[0]) {
+                return Integer.compare(a[0], b[0]);
+            } else {
+                return Integer.compare(a[1], b[1]);
+            }
+        });
+
+        return pairs;
+    }
+}
