c67018fa3544e9191b70f1397b492ea68a597180
virbaba <109612471+virbaba@users.noreply.github.com>
2023-10-27 15:49:51 +0530

    Create MinSortedRotated.java

---
+++ MinSortedRotated.java
@@ -0,0 +1,27 @@
+public class MinimumInRotatedSortedArray {
+
+    public static int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                right--;
+            }
+        }
+
+        return nums[left];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int min = findMin(nums);
+        System.out.println("The minimum element is: " + min);
+    }
+}
